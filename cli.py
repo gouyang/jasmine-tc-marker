f4c0f97fa0f3ef640daf020480163253603f9ab0
Radim Hrazdil <rhrazdil@redhat.com>
2020-04-20 13:50:52 +0200

    Fix parameters, not removing authentication cases

--- cli.py
+++ cli.py
@@ -4,8 +4,6 @@ import re
 import click
 from lxml import etree
 
-POLARION_PROJECT_ID = 'CNV'
-
 
 class Element:
     def __init__(self, tag, attributes):
@@ -20,35 +18,22 @@ def add_children(parent, children):
     return added_children
 
 
-def remove_login_testsuite(root):
-    """
-    Every openshift/console test suite starts with authentication and creates a new test namespace
-    These tests are included in the test report, as prerequisites for the remaining tests, however
-    not part of the actual test suite.
-    """
-    login_nodes = root.xpath(
-        "//testsuite[contains(@name,  'Auth test')  or  contains(@name,  'Create a test namespace')]"
-    )
-    for login_node in login_nodes:
-        login_node.getparent().remove(login_node)
-
-
-def add_testsuites_properties(xml, custom_plannedin, tier, env_storage, env_os, dry_run):
+def add_testsuites_properties(xml, project_id, plannedin, tier, env_storage, env_os, dry_run):
     properties = [
-        Element('property', {'name': 'polarion-project-id', 'value': POLARION_PROJECT_ID}),
+        Element('property', {'name': 'polarion-project-id', 'value': project_id}),
         Element('property', {'name': 'polarion-lookup-method', 'value': 'id'}),
-        Element('property', {'name': 'polarion-custom-plannedin', 'value': custom_plannedin}),
+        Element('property', {'name': 'polarion-custom-plannedin', 'value': plannedin}),
         Element(
             'property',
             {
                 'name': 'polarion-testrun-id',
-                'value': '_'.join([custom_plannedin, tier, env_storage, env_os])
+                'value': '_'.join([plannedin, tier, env_os, env_storage])
             }
         ),
         Element('property', {'name': 'polarion-custom-isautomated', 'value': 'True'}),
         Element('property', {'name': 'polarion-testrun-status-id', 'value': 'inprogress'}),
-        Element('property', {'name': 'polarion-custom-env-os', 'value': 'inprogress'}),
-        Element('property', {'name': 'polarion-custom-env-storage', 'value': 'inprogress'}),
+        Element('property', {'name': 'polarion-custom-env-os', 'value': env_os}),
+        Element('property', {'name': 'polarion-custom-env-storage', 'value': env_storage}),
         Element('property', {'name': 'polarion-dry-run', 'value': dry_run}),
     ]
     testsuites_properties = etree.SubElement(xml.getroot(), 'properties')
@@ -56,7 +41,6 @@ def add_testsuites_properties(xml, custom_plannedin, tier, env_storage, env_os, 
 
 
 def process_testcases(testcases):
-
     regex = re.compile(r'ID\(CNV-\d+\)\s*')
     for testcase in testcases:
         found = regex.search(testcase.get('name'))
@@ -69,45 +53,47 @@ def process_testcases(testcases):
             )
             # Remove the identifier from the testcase name
             testcase.set('name', regex.sub('', testcase.get('name')))
-        else:
-            raise Exception("Couldn't find test case identifier in '{}'".format(
-                testcase.get('name')
-            ))
 
 
 @click.command()
 @click.option('--report-path', help='path to the XML file with tests report', required=True)
 @click.option(
+    '--polarion-project',
+    help='Polarion project indentifier',
+    default='CNV',
+    show_default=True,
+)
+@click.option(
     '--custom-plannedin',
-    help='planned in attribute used for polarion-custom-plannedin. Example: 2_3',
-    required=True)
+    help='planned in attribute used for polarion-custom-plannedin.',
+    default='2_3',
+    show_default=True
+)
 @click.option(
     '--tier',
     help='tier of the test run',
     type=click.Choice(['tier1', 'tier2', 'tier3'], case_sensitive=False),
-    required=True
+    default='tier1',
+    show_default=True,
 )
-@click.option('--env-storage', help='storage class used for the test run', required=True)
+@click.option('--env-storage', help='storage class used for the test run', default='nfs', show_default=True)
 @click.option('--env-os', help='OS used on cluster nodes', default='RHCOS', show_default=True)
 @click.option(
     '--dry-run',
     help='use True for dry run, False by default',
     type=click.Choice(['True', 'False'], case_sensitive=False),
     default='False',
-    show_default=True
+    show_default=True,
 )
-def main(report_path, custom_plannedin, tier, env_storage, env_os, dry_run):
+def main(report_path, polarion_project, custom_plannedin, tier, env_storage, env_os, dry_run):
     xml = etree.parse(report_path)
 
-    # Remove authentication cases
-    remove_login_testsuite(xml)
+    # add test suite properties
+    add_testsuites_properties(xml, polarion_project, custom_plannedin, tier, env_storage, env_os, dry_run)
 
     # add and process testcase properties
     testcases = xml.xpath("//testcase")
     process_testcases(testcases)
-
-    # add test suite properties
-    add_testsuites_properties(xml, custom_plannedin, tier, env_storage, env_os, dry_run)
 
     # write the result to a file
     basepath, filename = path.split(report_path)
